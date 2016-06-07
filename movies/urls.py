2bb859eceefb0020d4740968ab795b4bc1e19f8b
Dhruv Thakur <dhruvt93@gmail.com>
2016-06-08 02:16:57 +0530

    Seen, Unseen, Bookmarked tabs added

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add/$',views.add_data, name='add_data'),
-    url(r'^change_seen_status/$', views.change_seen_status, name='change_seen_status')
+    url(r'^change_seen_status/$', views.change_seen_status, name='change_seen_status'),
+    url(r'^change_book_status/$', views.change_book_status, name='change_book_status'),
+    url(r'^bookmarked$', views.bookmarked, name='bookmarked'),
+    url(r'^seen$', views.seen_movies, name='seen_movies'),
+    url(r'^unseen$', views.unseen_movies, name='unseen_movies')
 ]
--- movies/views.py
+++ movies/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from .models import Movie
 import sqlite3
 from django.http import HttpResponse
-# Create your views here.
+
 def index(request):
     data = Movie.objects.all()[:100]
     # print data
     return render(request,'movies/index.html',{'data':data})
+
+def bookmarked(request):
+    data = Movie.objects.filter(bookmarked = True)
+    return render(request, 'movies/index.html',{'data':data})
+
+def seen_movies(request):
+    data = Movie.objects.filter(seen_status = True)
+    return render(request, 'movies/index.html',{'data':data})
+
+def unseen_movies(request):
+    data = Movie.objects.filter(seen_status = False)
+    return render(request, 'movies/index.html',{'data':data})
 
 def change_seen_status(request):
     id_to_change = request.POST['id']
@@ -17,6 +29,19 @@ def change_seen_status(request):
         m.seen_status = False
     else:
         m.seen_status = True
+    m.save()
+    # print m.title
+    return HttpResponse("done")
+
+def change_book_status(request):
+    id_to_change = request.POST['id']
+    pk = id_to_change.split('-')[1]
+    print id_to_change
+    m = Movie.objects.get(pk = int(pk))
+    if m.bookmarked == True:
+        m.bookmarked = False
+    else:
+        m.bookmarked = True
     m.save()
     # print m.title
     return HttpResponse("done")
